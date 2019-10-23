c5fd7e97118d5f1732d6b3e15a065767b21a5681
paul.balanche <pb@buzzbrothers.ch>
2019-10-23 11:10:22 +0200

    Some improvements

--- inc/classes/SignIn.php
+++ inc/classes/SignIn.php
@@ -9,8 +9,7 @@ class SignIn {
 
     private static $_instance;
 
-    private $client,
-        $signin_password = 'googleoauth';
+    private $client;
 
 
 
@@ -45,12 +44,30 @@ class SignIn {
 	 */
 	public function create_hooks() {
 
+        // Enqueue styles & scripts
+        add_action('login_enqueue_scripts', array( __CLASS__, 'login_enqueue_scripts' ) );
+
         // Hook google_oauth action in order to create or logged user
         add_action( 'admin_post_nopriv_google_oauth', array($this, 'sign_client') );
 
         // Add Google Signin button on login admin page
-        add_action( 'login_footer', array($this, 'display_link_signin') );
+        add_action( 'login_message', array($this, 'display_link_signin') );
+        add_filter( 'wp_login_errors', array($this, 'wp_login_errors') );
+
+        add_filter( 'get_avatar_data', array($this, 'get_avatar_data'), 10, 2 );
+        add_filter( 'user_profile_picture_description', array($this, 'user_profile_picture_description'), 10, 2 );
     }
+
+
+
+    /**
+	 * Wordpress Enqueues functions
+	 *
+	 */
+	public static function login_enqueue_scripts() {
+
+		wp_enqueue_style( 'wpegoogleoauth_admin_style', PLUGIN_ASSETS_URL . 'style/admin/style.min.css', false, true );
+	}
 
 
 
@@ -61,9 +78,6 @@ class SignIn {
     public function init_google_client() {
 
         $this->client = new \Google_Client();
-
-        // Set application name
-        $this->client->setApplicationName( 'Application name' );
 
         // Define scope
         $this->client->addScope( 'email' );
@@ -100,35 +114,54 @@ class SignIn {
                 // Create username
                 $user_name = strtolower($google_account_info->givenName) . '.' . strtolower($google_account_info->familyName);
 
-                // Check if user_name and email already exists or not
-                if ( ! username_exists($user_name) && ! email_exists($google_account_info->email) ) {
+                $user_id_from_username = username_exists($user_name);
+                $user_id_from_email = email_exists($google_account_info->email);
+
+                // // Check if user_name and email already exists or not
+                if( ! $user_id_from_username && ! $user_id_from_email ) {
 
                     $user_id = wp_insert_user( [
                         'user_email'    => $google_account_info->email,
                         'user_login'    => $user_name,
-                        'user_pass'     => $this->user_password(),
+                        'user_pass'     => wp_generate_password(),
                         'first_name'    => $google_account_info->givenName,
                         'last_name'     => $google_account_info->familyName,
                         'display_name'  => $google_account_info->name
                     ] );
-                    // $google_account_info->name->picture
                 }
+                else if( $user_id_from_username == $user_id_from_email ) {
 
-                // Now authenticates the user             
-                $user = wp_signon( [
-                    'user_login'    => $google_account_info->email,
-                    'user_password' => $this->user_password(),
-                    'remember'      => false
-                ], false );
+                    $user_id = wp_insert_user( [
+                        'ID'            => $user_id_from_username,
+                        'user_email'    => $google_account_info->email,
+                        'user_login'    => $user_name,
+                        'first_name'    => $google_account_info->givenName,
+                        'last_name'     => $google_account_info->familyName,
+                        'display_name'  => $google_account_info->name
+                    ] );
+                }
+                else {
+                    $error_signin = "Username and email already exist and doesn't match...";
+                }
+                
+                // Log in user by setting authentication cookies.
+                if( isset($user_id) ) {
+                    
+                    if( is_wp_error( $user_id ) )
+                        $error_signin = $user_id->get_error_message();
+                    else {
 
-                // Display errors if there are
-                if ( is_wp_error( $user ) ) {
-                    echo $user->get_error_message();
-                    exit;
+                        update_user_meta( $user_id, '_wpegoogleoauth_signin', true );
+                        update_user_meta( $user_id, '_wpegoogleoauth_picture', $google_account_info->picture );
+
+                        wp_set_auth_cookie( $user_id );
+                    }
+                        
                 }
 
                 // Redirect admin page
-                wp_safe_redirect( admin_url() );
+                $url_redirect = ( ! empty($error_signin) ) ? add_query_arg( 'error', urlencode($error_signin), wp_login_url()) : admin_url();
+                wp_safe_redirect($url_redirect);
                 exit;
             }
         }
@@ -140,24 +173,61 @@ class SignIn {
      * Add Google Signin button on login admin page
      * 
      */
-    static public function display_link_signin() {
+    static public function display_link_signin( $message ) {
 
         // Init Google client
         $this->init_google_client();
 
         // Request authorization from the user.
         $authUrl = $this->client->createAuthUrl();
-        echo '<div style="width: 240px;margin: auto;margin-top: 2rem;text-align: center;"><a href="' . $authUrl . '" class="button button-hero">Login with Google</a></div>';
+
+        $message .= '<div id="wpe_google_oauth_signin_container"><a href="' . $authUrl . '"></a></div>';
+
+        return $message;
     }
-    
+
 
 
     /**
-     * Get user secured password
+     * Hook wp_login_errors in order to display error during Google Signin
+     * 
      */
-    public function user_password() {
+    static public function wp_login_errors( $errors ) {
 
-        return md5( $this->signin_password );
+        if( isset($_GET['error']) && ! empty($_GET['error']) )
+            $errors->add( '', stripslashes(urldecode($_GET['error'])), 'message' );
+        
+            return $errors;
+    }
+
+
+
+    /**
+     * Hook get_avatar_data in order to return Google account photo
+     * 
+     */
+    static public function get_avatar_data( $args, $id_or_email ) {
+
+        $picture = get_user_meta( $id_or_email, '_wpegoogleoauth_picture', true );        
+        if( $picture && ! empty($picture) && ! is_null($picture) )
+            $args['url'] = $picture;
+
+        return $args;
+    }
+
+
+
+    /**
+     * Hook user_profile_picture_description in order to return specific description
+     * 
+     */
+    static public function user_profile_picture_description( $description, $profileuser ) {
+
+        $picture = get_user_meta( $profileuser->ID, '_wpegoogleoauth_picture', true );        
+        if( $picture && ! empty($picture) && ! is_null($picture) )
+            $description = 'This is your Google account photo.';
+
+        return $description;
     }
 
 
