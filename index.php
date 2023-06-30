ccb1fc65cda3bf3b160473fe92ed2464e556c14c
Paul Balanche <pb@buzzbrothers.ch>
2023-06-30 10:55:20 +0200

    Up

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 * Plugin Name: WPE Google Sign-In
 * Description: Connect to the dashboard easily with Google Sign-In
 * Text Domain: WpeGoogleSignIn
-* Version: 1.1.4
+* Version: 1.1.5
 * Author: Paul Balanche
 **/
 
